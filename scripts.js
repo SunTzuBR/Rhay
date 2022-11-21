d32644e84ee1bc5fc8d78c6e7410d73c7908e889
SunTzuBR <96428441+SunTzuBR@users.noreply.github.com>
2022-11-21 11:52:01 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -101,3 +101,18 @@ function typeWriter(p){
 setInterval(() => typeWriter(p), 50000);
 }
 typeWriter(frase);
+
+document.addEventListener("mousemove", function(e)
+{var body=document.querySelector('body');
+var heart=document.createElement("span");
+var x=e.offsetX;
+var y=e.offsetY;
+heart.style.left=x+'px';
+heart.style.top=y+'px';
+body.appendChild(heart);
+
+setTimeout(function(){
+  heart.remove();
+},2000)
+
+})
